52ae68e1de667a42642089e14fd226b26c987e45
Renjiro Kazuki <cihuyh22@gmail.com>
2026-06-10 18:59:55 +0800

    Implement repeatString and enable repeatString tests

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function(text, num) {
+    if ( num < 0 ) {
+        return "ERROR";
+    }
+    let hasil = "";
+    if (num > 0) {
+        for (let i = 0; i < num; i++) {
+            hasil += text;
+        }
+        
+    };
+    
+    return hasil;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
