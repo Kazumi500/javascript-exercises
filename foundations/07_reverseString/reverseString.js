9c45337d75a74af7e1e2984ce1fcc01e3cf17976
Renjiro Kazuki <cihuyh22@gmail.com>
2026-06-10 20:12:40 +0800

    Implement reverseString and enable reverseString tests

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    const textSpit = text.split('').reverse().join("");
+    return textSpit;
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
