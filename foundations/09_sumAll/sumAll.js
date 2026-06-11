a695ec6ee545eeb42b298e00164cc409e82deac5
Renjiro Kazuki <cihuyh22@gmail.com>
2026-06-11 18:51:36 +0800

    Implement SumALL and enable all test

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
+    const sumAll = function(num1, num2) {
+        if (typeof num1 !== 'number' || typeof num2 !== 'number' || num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2) ){
+        return "ERROR";
+            
+        };
+        let total = 0;
+        let angkaKecil = Math.min(num1, num2);
+        let angkaBesar = Math.max(num1, num2);
 
-};
+        for (let i = angkaKecil; i <= angkaBesar; i++){
+            total += i;
+        };
+        return total;
+    };
+
 
 // Do not edit below this line
 module.exports = sumAll;
